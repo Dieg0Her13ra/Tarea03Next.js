38b6ec7438880c957a04372f09be4e1bb542502c
AusDr4ko! <diego0hh013@gmail.com>
2026-04-06 04:57:13 +0000

    Rutas anidadas y el contenido en ellas

---
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -0,0 +1,4 @@
+export default function InvoicesPage(){
+    return <p>Invoices Page. Secret message
+         "Nunca busques pareja en un bar", consejo sabio del oficial de supercool</p>
+}
--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -8,9 +8,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${montserrat.className} antialiased`}>
+        Esto es parte del layout
         <h1>Este titulazo fijate nomas wow increible</h1>
         <div style= {{border: '1px solid #000'}}> </div>
         {children}
+        <footer className='py=10 flex justify-center items-center'> 
+          hecho con destrucción y caos más un toque de maldad
+        </footer>
         </body>
     </html>
   );
